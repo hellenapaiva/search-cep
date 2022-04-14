5da0414fd85e7ac0c4f9603ec4cb71c91efd6715
hellenapaiva <hellenapaiva@msn.com>
2022-04-14 14:11:15 -0300

    integrations API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { BiSearchAlt } from "react-icons/bi";
+import api from "./services/api";
 
 function App() {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
+  const [saveCep, setSaveCep] = useState({});
+
+  async function handleSearch(event) {
+    event.preventDefault();
+    if (input === "") {
+      alert("Digite o CEP que deseja buscar...");
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`);
+      setSaveCep(response.data);
+    } catch {
+      console.error("Erro ao buscar CEP.");
+      setInput("");
+    }
+  }
+
+  console.log();
 
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP..." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <BiSearchAlt size={25} color="#fff" />
         </button>
       </div>
 
       <main className="main">
-        <h2>CEP: 79003222 </h2>
-        <span> Rua Só testando</span>
-        <span> Complemento: Algum complemento</span>
-        <span> Bairro: Vila Rosa</span>
-        <span>Campo Grande - MS</span>
+        <h2>CEP: {saveCep.cep} </h2>
+        <span> {saveCep.logradouro}</span>
+        <span>{saveCep.complemento}</span>
+        <span>{saveCep.bairro}</span>
+        <span>
+          {saveCep.localidade} - {saveCep.uf}
+        </span>
       </main>
     </div>
   );
